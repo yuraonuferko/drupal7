d9a552eeef49953151c5fc496c2b7898f41af848
Yurii Onuferko <yura_onuferko@yahoo.com>
2016-11-03 11:18:07 +0200

    heder end footer complete

--- sites/all/themes/bootstrap/yonuferko/templates/page.tpl.php
+++ sites/all/themes/bootstrap/yonuferko/templates/page.tpl.php
@@ -9,7 +9,9 @@
 </div>
 <div  id="Before-Header-right">
     <?php print render($page['before_header_right']); ?>
-  <a href="#"><img src="/sites/all/themes/bootstrap/yonuferko/img/korz.jpg" alt="Пример"></a>
+  <!--<a href="#"><img src="/sites/all/themes/bootstrap/yonuferko/img/korz.jpg" alt="Пример"></a>
+-->
+    <button href="#" ></buttonhr>
 </div>
 <div id="Header">
     <?php print render($page['header']); ?>
@@ -47,6 +49,42 @@
 <div id="After-Main">
     <?php print render($page['after-main']); ?>
 </div>
-<div id="Footer">
-    <?php print render($page['footer']); ?>
-</div>
+
+    <div  id="Footer-Left">
+        <?php print render($page['footer-left']); ?>
+        <h2>Cлужба поддержки</h2>
+        <ul>
+            <li>Проверка статуса заказа</li>
+            <li>Способы доставки</li>
+            <li>Возврат и обмен</li>
+            <li>Онлайн чат поддержка</li>
+            <li>Подарочные сертификаты</li>
+        </ul>
+    </div>
+    <div  id="Footer">
+        <?php print render($page['footer']); ?>
+        <h2>Информация</h2>
+        <ul>
+            <li>О компании </li>
+            <li>Гарантия</li>
+            <li>mr.Fix</li>
+            <li>Помощь</li>
+            <li>Производители</li>
+        </ul>
+    </div>
+    <div  id="Footer-Right">
+        <?php print render($page['footer-right']); ?>
+        <h2>Контакты</h2>
+        <ul>
+            <li> &#10004г. Киев, Бесарабская пл.  </li>
+            <li>&#xe011info@mrlook.com.ua</li>
+            <li>&#9990(044) 24 799 43</li>
+            <li>&#9990(044) 24 799 34<li>
+    </div>
+    <div  id="Footer-Right1">
+        <?php print render($page['footer-right1']); ?>
+        <h2>Присоединяйтесь</h2>
+        <li>Доступ к информации о подарках и новинках<li>
+            <input placeholder ='&#xe011' type="text">
+        <li>Социальные сети<li>
+    </div>
