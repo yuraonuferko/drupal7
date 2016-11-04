186585798a6d4209c0658cb8c8282e0adadb9c1b
Yurii onuferko <yura_onuferko@yahoo.com>
2016-11-04 15:39:01 +0200

    04_11_16

--- sites/all/themes/bootstrap/yonuferko/templates/page.tpl.php
+++ sites/all/themes/bootstrap/yonuferko/templates/page.tpl.php
@@ -9,23 +9,24 @@
 </div>
 <div  id="Before-Header-right">
     <?php print render($page['before_header_right']); ?>
-  <!--<a href="#"><img src="/sites/all/themes/bootstrap/yonuferko/img/korz.jpg" alt="Пример"></a>
--->
-    <button href="#" ></buttonhr>
+
+    <button href="#" ></button>
 </div>
 <div id="Header">
     <?php print render($page['header']); ?>
     <!-- visible logo-->
-    <?php if ($title == ""): ?>
-      <img  src="/sites/all/themes/bootstrap/yonuferko/img/logo2.png" alt="logo">
+    <?php //if ($title == ""): ?>
+     <!-- <img  src="/sites/all/themes/bootstrap/yonuferko/img/logo2.png" alt="logo">
     <input placeholder="Поиск">
-     </input>
-    <?php endif; ?>
+     </input>  -->
+    <?php //endif; ?>
 </div>
+
 <div id="Slider">
     <?php print render($page['slider']); ?>
-    <?php print $title; ?>
+    <?php //print $title; ?>
 </div>
+
 <div id="Before-Main">
     <?php print render($page['before-main']); ?>
 </div>
@@ -50,41 +51,47 @@
     <?php print render($page['after-main']); ?>
 </div>
 
-    <div  id="Footer-Left">
-        <?php print render($page['footer-left']); ?>
-        <h2>Cлужба поддержки</h2>
-        <ul>
-            <li>Проверка статуса заказа</li>
-            <li>Способы доставки</li>
-            <li>Возврат и обмен</li>
-            <li>Онлайн чат поддержка</li>
-            <li>Подарочные сертификаты</li>
-        </ul>
-    </div>
-    <div  id="Footer">
-        <?php print render($page['footer']); ?>
-        <h2>Информация</h2>
-        <ul>
-            <li>О компании </li>
-            <li>Гарантия</li>
-            <li>mr.Fix</li>
-            <li>Помощь</li>
-            <li>Производители</li>
-        </ul>
-    </div>
-    <div  id="Footer-Right">
-        <?php print render($page['footer-right']); ?>
-        <h2>Контакты</h2>
-        <ul>
-            <li> &#10004г. Киев, Бесарабская пл.  </li>
-            <li>&#xe011info@mrlook.com.ua</li>
-            <li>&#9990(044) 24 799 43</li>
-            <li>&#9990(044) 24 799 34<li>
-    </div>
-    <div  id="Footer-Right1">
-        <?php print render($page['footer-right1']); ?>
-        <h2>Присоединяйтесь</h2>
-        <li>Доступ к информации о подарках и новинках<li>
-            <input placeholder ='&#xe011' type="text">
-        <li>Социальные сети<li>
-    </div>
+<div  id="Footer">
+  <?php print render($page['footer']); ?>
+  <table>
+  <tr>
+    <th>Cлужба поддержки</th>
+    <th>Информация</th>
+    <th>Контакты</th>
+    <th>Присоединяйтесь</th>
+  </tr>
+  <tr>
+      <td> Проверка и статус заказа</td>
+      <td>О компании</td>
+      <td>&#10004г. Киев, Бесарабская пл.</td>
+    <td>Доступ к информации о подарках и новинках</td>
+  </tr>
+  <tr>
+      <td>Способы доставки</td>
+      <td>Гарантия</td>
+      <td>&#xe011info@mrlook.com.ua</td>
+    <td><input placeholder ='&#xe011' type="text"></td>
+  </tr>
+  <tr>
+    <td>Возврат и обмен </td>
+    <td>mr.Fix</td>
+    <td>&#9990(044) 24 799 43</td>
+    <td>Социальные сети</td>
+  </tr>
+  <tr>
+    <td>Онлайн чат поддержка</td>
+    <td>Помощь</td>
+    <td>&#9990(044) 24 799 43</td>
+  </tr>
+  <tr>
+    <td> Подарочные сертификаты</td>
+    <td>Производители</td>
+    <td></td>
+  </tr>
+</table>
+
+
+
+
+  </div>
+
